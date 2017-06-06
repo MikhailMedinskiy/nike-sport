30b4bf5031afa5c943504638ebedd8c8fcb51532
MedinskiyMikhail <medinskiym@gmail.com>
2017-06-06 16:12:00 +0300

    add all style and html(not responsive)

---
+++ js/common.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+
+    $(document).ready(function () {
+
+        $(".list-color__item").on( "click", function(){
+            $(".list-color__item").removeClass("list-color__item--active");
+            $(this).addClass("list-color__item--active");
+            var bgcolor = $(this).css('background');
+            $(".product").css("background", bgcolor);
+
+        });
+    });
+
+   
+
+})(jQuery);
